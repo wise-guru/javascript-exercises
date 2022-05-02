9de9012d5337ed4b56870ab5f7fdbde160517500
wise-guru <wise.guru123@gmail.com>
2022-05-02 00:03:44 -0400

    Complete first and second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+function repeatString(userString, loopNumber) {
+let repeatedString = ("");
+    if (loopNumber < 0) return 'ERROR';
 
-};
+    while (loopNumber > 0) {
+        repeatedString += userString;
+        loopNumber--;
+    } 
+    return repeatedString; 
+
+
+}
 
 // Do not edit below this line
 module.exports = repeatString;
