1b2f4e632bb76634b2d984c86f9f111c8539d97c
wise-guru <wise.guru123@gmail.com>
2022-05-03 19:27:15 -0400

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let splitString = str.split("");
+
+    let reverseArray = splitString.reverse("");
+
+    let finalString = reverseArray.join("");
+    return finalString;
 };
 
 // Do not edit below this line
