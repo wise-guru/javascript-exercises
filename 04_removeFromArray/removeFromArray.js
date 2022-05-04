6c86c050bcba2a3d6086c68ddde605382ae8b94e
wise-guru <wise.guru123@gmail.com>
2022-05-03 22:26:20 -0400

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...orig) {
+    const array = orig[0];
 
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!orig.includes(item)) {
+            newArray.push(item)
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
