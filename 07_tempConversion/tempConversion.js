345de5d35464b54ae7043925a15095f8db883d8d
wise-guru <wise.guru123@gmail.com>
2022-05-05 21:58:21 -0400

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fNum) {
 
+let result = ([fNum] - 32) * (5/9);
+let celsiusConversion = Math.round(result * 10) / 10;
+return celsiusConversion;
 };
 
-const ctof = function() {
+const ctof = function(cNum) {
+let result = [cNum] * (9/5) + 32;
 
+let farenheitConversion = Math.round(result * 10) / 10;
+return farenheitConversion;
 };
 
 // Do not edit below this line
